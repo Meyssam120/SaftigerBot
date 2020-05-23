1439d97e65dc9cfcab062798a23afd26261c7d1a
Meyssam120 <60116852+meyssam120@users.noreply.github.com>
2020-05-23 13:32:33 +0200

    removed pvt

--- src/main/java/Private.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Meyssam Saghiri on Mai 23, 2020
- */
-public class Private {
-
-    static String token = "NzA0NjQ1MzMyMDQ2MzE1NTUw.XqgLng.-ZIl7o_50jNvp6JX29DR2B5yUa";
-    static String msmID = "232060595011059714";
-
-}
