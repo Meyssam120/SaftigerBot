d0046dc1284e9b2adbb298c05d81cb6dea51847d
Meyssam120 <60116852+Meyssam120@users.noreply.github.com>
2020-05-23 13:26:08 +0200

    Prv

--- src/main/java/Private.java
+++ src/main/java/Private.java
@@ -3,7 +3,7 @@
  */
 public class Private {
 
-    static String token = "NzA0NjQ1MzMyMDQ2MzE1NTUw.XqgLng.-ZIl7o_50jNvp6JX29DR2B5yUaw";
+    static String token = "NzA0NjQ1MzMyMDQ2MzE1NTUw.XqgLng.-ZIl7o_50jNvp6JX29DR2B5yUa";
     static String msmID = "232060595011059714";
 
 }
